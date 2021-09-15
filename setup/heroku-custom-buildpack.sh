822ea0899a2ffac6bd9c7417500dc9ea81477dc1
Messias Dias <messiasdias.ti@gmail.com>
2021-09-15 12:53:27 -0300

    Atualiza heroku scripts

--- setup/heroku-custom-buildpack.sh
+++ setup/heroku-custom-buildpack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp application/config/heroku.database.php application/config/database.php
 cp application/config/heroku.config.php application/config/config.php
-chmod -R 775 assets/uploads
+chmod -R 777 assets/uploads
 #chown -R root:www-data assets/uploads
+
+echo "Run heroku-custom-buildpack.sh"
