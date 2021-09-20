c71c3b4efc52224c70184da8ae58796f5df067e1
Messias Dias <messiasdias.ti@gmail.com>
2021-09-20 04:16:37 -0300

    De acordo com a issue #44

--- application/modules/endereco_contato/views/contato_form_fields.php
+++ application/modules/endereco_contato/views/contato_form_fields.php
@@ -17,20 +17,20 @@
         <label for="<?php echo $telefone; ?>" class="form-control-label">Telefone</label>
     </div>
     <div class="col-12 col-md-3">
-        <input type="text" id="<?php echo $telefone; ?>" name="<?php echo $telefone; ?>" placeholder="Telefone" class="telefone form-control" value="<?php if(isset($detalhes) && isset($detalhes->$telefone)){ echo $detalhes->$telefone; } ?>">
+        <input type="text" id="<?php echo $telefone; ?>" name="<?php echo $telefone; ?>" placeholder="(41) 4040-4676" class="telefone form-control" value="<?php if(isset($detalhes) && isset($detalhes->$telefone)){ echo $detalhes->$telefone; } ?>">
     </div>
 
     <div class="col col-md-1">
         <label for="<?php echo $celular; ?>" class="form-control-label">Celular</label>
     </div>
     <div class="col-12 col-md-3">
-        <input type="text" id="<?php echo $celular; ?>" name="<?php echo $celular; ?>" placeholder="Celular" class="celular form-control" value="<?php if(isset($detalhes) && isset($detalhes->$celular)){ echo $detalhes->$celular; } ?>" required="required">
+        <input type="text" id="<?php echo $celular; ?>" name="<?php echo $celular; ?>" placeholder="(41) 9 9182-1872" class="celular form-control" value="<?php if(isset($detalhes) && isset($detalhes->$celular)){ echo $detalhes->$celular; } ?>" required="required">
     </div>
 
     <div class="col col-md-1">
         <label for="<?php echo $email; ?>" class="form-control-label">Email</label>
     </div>
     <div class="col-12 col-md-3">
-        <input type="email" id="<?php echo $email; ?>" name="<?php echo $email; ?>" placeholder="Email" class="form-control" value="<?php if(isset($detalhes) && isset($detalhes->$email)){ echo $detalhes->$email; } ?>">
+        <input type="email" id="<?php echo $email; ?>" name="<?php echo $email; ?>" placeholder="seuemail@exemplo.com.br" class="form-control" value="<?php if(isset($detalhes) && isset($detalhes->$email)){ echo $detalhes->$email; } ?>">
     </div> 
 </div>
