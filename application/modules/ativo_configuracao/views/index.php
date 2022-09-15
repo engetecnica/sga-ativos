fb971b5014027e2e8dc6ece714255076a54b8db0
Messias Dias <messiasdias.ti@gmail.com>
2022-09-14 22:50:12 -0300

    Adiciona datatables paginacao no modulo ativo_configuracao (#122)

--- application/modules/ativo_configuracao/views/index.php
+++ application/modules/ativo_configuracao/views/index.php
@@ -16,77 +16,10 @@
                 <div class="col-lg-12">
                     <h2 class="title-1 m-b-25">Configurações de Ativos</h2>
                     <div class="table-responsive table--no-card m-b-40">
-                        <table class="table table-borderless table-striped table-earning" id="ativo_configuracao_index">
-                            <thead>
-                                <tr>
-                                    <th width="7%">Id</th>
-                                    <th>Titulo</th>
-                                    <th>Categoria</th>
-                                    <th>Situação</th>
-                                    <th class="text-right">Gerenciar</th>
-                                </tr>
-                            </thead>
-                            <!-- <tbody>
-                                <?php foreach($lista as $valor){ ?>
-                                <tr id="<?php echo "configuracao-{$valor->id_ativo_configuracao}"?>">
-                                    <td>
-                                        <?php if ($valor->permit_edit || $valor->permit_delete) {?>
-                                            <a  href="<?php echo base_url('ativo_configuracao'); ?>/editar/<?php echo $valor->id_ativo_configuracao; ?>">
-                                                <?php echo $valor->id_ativo_configuracao; ?>
-                                            </a>
-                                        <?php } else { echo $valor->id_ativo_configuracao;  } ?>
-                                    </td>
-                                    <td><?php echo $valor->titulo; ?></td>
-                                    <td><?php echo ($valor->id_ativo_configuracao_vinculo=='') ? "Configuração Principal" : $valor->id_ativo_configuracao_vinculo; ?></td>
-                                    <td>
-                                      <?php $situacao = $this->get_situacao($valor->situacao, 'DESCARTADO', 'secondary');?>
-                                      <span class="badge badge-<?php echo $situacao['class']; ?>"><?php echo $situacao['texto']; ?></span>
-                                    </td>
-
-                                    <td class="text-right">
-
-                                    <?php if ($valor->permit_edit || $valor->permit_delete) {?>
-                                        <div class="btn-group">
-                                            <button 
-                                                class="btn btn-secondary btn-sm dropdown-toggle" 
-                                                type="button"
-                                                data-toggle="dropdown" 
-                                                aria-haspopup="true" 
-                                                aria-expanded="false"
-                                            >
-                                                Gerenciar
-                                            </button>
-                                            <div class="dropdown-menu">
-                                                <?php if ($valor->permit_edit == 1) {?>
-                                                <a 
-                                                    class="dropdown-item " 
-                                                    href="<?php echo base_url('ativo_configuracao'); ?>/editar/<?php echo $valor->id_ativo_configuracao; ?>"
-                                                >
-                                                    <i class="fas fa-edit"></i> Editar
-                                                </a>
-                                                <?php } ?>
-                                                <?php if ($valor->permit_delete == 1) {?>
-                                                <div class="dropdown-divider"></div>
-                                                <a 
-                                                    class="dropdown-item  deletar_registro" 
-                                                    href="javascript:void(0)" 
-                                                    data-href="<?php echo base_url('ativo_configuracao'); ?>/deletar/<?php echo $valor->id_ativo_configuracao; ?>" 
-                                                    data-registro="<?php echo $valor->id_ativo_configuracao;?>" 
-                                                    data-tabela="ativo_configuracao"
-                                                >
-                                                    <i class="fas fa-trash"></i> Excluir
-                                                </a>
-                                                <?php } ?>
-                                            </div>
-                                            <?php } else { ?>
-                                                -
-                                            <?php } ?>
-                                        </div>
-                                    </td>
-                                </tr>
-                               <?php } ?>
-                            </tbody> -->
-                        </table>
+                        <table 
+                            class="table table-borderless table-striped table-earning" 
+                            id="ativo_configuracao_index"
+                        ></table>
                     </div>
                 </div>
             </div>
