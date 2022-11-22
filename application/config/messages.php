83e5c2adf8be188efa56fb227808319dad4ccd9a
André Baill <srandrebaill@gmail.com>
2022-11-22 12:41:10 -0300

    Alteração no Ferramental Estoque para selecionar qual é o patrimônio que será disponibilizado

---
+++ application/config/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function messages_fallback()
+{
+
+
+    return [
+        /* Login */
+        'login_sucesso' => 'Login efetuado com sucesso!',
+        'login_incorreto' => 'Usuário ou senha incorreto. Tente novamente!',
+        'login_inativo' => 'Usuário inativo, favor contatar um administrador do Sistema!'
+
+        /* Cadastros */
+        
+    ];
+
+
+
+}
