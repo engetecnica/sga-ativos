9cad7d8cbd7683beeb8b011899ee24bcbde0ed20
srandebaill <srandrebaill@gmail.com>
2022-11-24 18:31:40 -0300

    Ferramental Requisição sem filtro nos ativos lista

--- application/modules/ferramental_requisicao/controllers/Ferramental_requisicao.php
+++ application/modules/ferramental_requisicao/controllers/Ferramental_requisicao.php
@@ -77,9 +77,15 @@ class Ferramental_requisicao  extends MY_Controller {
 
     # Criar uma nova Requisição
     function adicionar() {
+        $grupos = [];
+        if ($this->user->nivel == 1) {
+           $grupos = $this->ativo_externo_model->get_grupos($this->user->id_obra);
+        }
         
-        $grupos = $this->ativo_externo_model->get_grupos(null);
-        
+        if ($this->user->nivel == 2) {
+           $grupos = $this->ativo_externo_model->get_grupos($this->user->id_obra);
+        }
+    
         $this->get_template('index_form',[
             'grupos' => $grupos,
             'obras' => $this->obra_model->get_obras(),
