6e89c50a17e438466f8120134556a67740bdb83c
user.name: André Baill <user.email: srandrebaill@gmail.com>
2023-04-24 15:53:43 -0300

    Versão Estável - SGA 2023

--- setup/automations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TODAY=`date +%c`
-echo \ >> /var/www/sgaeng/log/automacoes.log
-echo $TODAY >>  /var/www/sgaeng/log/automacoes.log
-curl https://www.sga-e.eng.br/app/automacoes >> /var/www/sgaeng/log/automacoes.log
-echo \ >> /var/www/sgaeng/log/automacoes.log
