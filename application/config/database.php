54f68a1415cc7bc4ef5ac09e8c1c1c1d2f35a649
user.name: André Baill <user.email: srandrebaill@gmail.com>
2023-04-24 16:18:16 -0300

    Conexão com Banco de Dados

--- application/config/database.php
+++ application/config/database.php
@@ -72,12 +72,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 $active_group = 'default';
-$db['default'] = array(
+$db['localhost'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'root123',
 	'database' => 'engeativos',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '107.161.183.195',
+	'username' => 'sgaeng_sistema',
+	'password' => 'ZUFFMG7QTUIC',
+	'database' => 'sgaeng_sistema',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
